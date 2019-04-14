f5755d4acc1e7df3e9933a31d02bf7b0e6cf192f
gongbiao <developbiao@gmail.com>
2019-04-15 07:45:34 +0800

    Add captcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">用户注册</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal register-page" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
