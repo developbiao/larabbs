814c29a1517436e418f479bd75d378889514ed81
gongbiao <developbiao@gmail.com>
2019-05-10 08:07:56 +0800

    Seeding topics and users data

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,16 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    // random a month date
+    $updated_at = $faker->dateTimeThisMonth();
+    // created time less updated time
+    $created_at = $faker->datetimeThisMonth($updated_at);
     return [
-        // 'name' => $faker->name,
+        'title'      => $faker->sentence,
+        'body'       => $faker->text(),
+        'excerpt'    => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at
     ];
 });
--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
 
@@ -7,10 +9,19 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        // get all user ids
+        $user_ids = User::all()->pluck('id')->toArray();
+        // get all category ids
+        $category_ids = Category::all()->pluck('id')->toArray();
+        // get faker instance
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+            ->times(100)
+            ->make()
+            ->each(function ($topic, $index) use($user_ids, $category_ids, $faker) {
+                $topic->user_id   = $faker->randomElement($user_ids);
+                $topic->category_id = $faker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
