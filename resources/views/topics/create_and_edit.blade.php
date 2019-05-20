19666fb89d6626acc0bd58a8b9590fc8ec49b09d
gongbiao <developbiao@gmail.com>
2019-05-20 08:09:43 +0800

    MYSIWYG Editor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    @section('styles')
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+    @stop
 
     <div class="container">
         <div class="col-md-10 col-md-offset-1">
@@ -54,5 +57,19 @@
             </div>
         </div>
     </div>
+    @section('scripts')
+        <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
+        <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
+        <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
+        <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+
+        <script>
+            $(document).ready(function(){
+                var editor = new Simditor({
+                    textarea: $('#editor'),
+                });
+            });
+        </script>
+    @stop
 
 @endsection
