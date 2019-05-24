d7a452654b4c8e55854f2384eb9f78ecac03af89
gongbiao <developbiao@gmail.com>
2019-05-24 08:35:31 +0800

    Implement Auto translate slug

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -42,5 +42,9 @@ class Topic extends Model
        return $query->orderBy('updated_at', 'desc');
     }
 
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge( [$this->id, $this->slug], $params ) );
+    }
 
 }
