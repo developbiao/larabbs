9fad27da8626553419f054073bb60427c0f4df84
gongbiao <developbiao@gmail.com>
2019-08-25 12:47:12 +0800

    verificationCode store

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\VerificationCodeRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 use Overtrue\EasySms\EasySms;
 use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
 
@@ -14,16 +15,22 @@ class VerificationCodesController extends Controller
     {
         $phone = $request->phone;
 
-        // generate 4 random number
-        $code = str_pad( random_int(1, 9999), 4, 0, STR_PAD_LEFT );
-
-        try{
-            $result = $easySms->send($phone, [
-               'content'  => "【Skill社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
-            ]);
-        } catch (NoGatewayAvailableException $exception){
-            $message = $exception->getException('yunpian')->getMessage();
-            return $this->response->errorInternal($message ?: '短信发送异常');
+        if(!app()->environment('production'))
+        {
+           $code = '1234';
+        }
+        else
+        {
+            // generate 4 random number
+            $code = str_pad( random_int(1, 9999), 4, 0, STR_PAD_LEFT );
+            try{
+                $result = $easySms->send($phone, [
+                    'content'  => "【Skill社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (NoGatewayAvailableException $exception){
+                $message = $exception->getException('yunpian')->getMessage();
+                return $this->response->errorInternal($message ?: '短信发送异常');
+            }
         }
 
         $key = 'verificationCode_' . str_random(15);
